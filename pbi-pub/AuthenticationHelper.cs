4af4af29cfc23b0413661c121192b6ad67dbf7f9
devkeydet <devkeydet@gmail.com>
2018-03-08 16:58:48 -0500

    init

---
+++ pbi-pub/AuthenticationHelper.cs
@@ -0,0 +1,45 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pbi_pub
+{
+    internal static class AuthenticationHelper
+    {
+        internal static string GetAccessToken(string clientId, string clientSecret, string username, string password)
+        {
+            const string resource = "https://analysis.windows.net/powerbi/api";
+
+            using (var authClient = new HttpClient())
+            {
+                var authContent = new FormUrlEncodedContent(new[]
+                {
+                    new KeyValuePair<string, string>("resource", resource),
+                    new KeyValuePair<string, string>("client_id", clientId),
+                    new KeyValuePair<string, string>("grant_type", "password"),
+                    new KeyValuePair<string, string>("username", username),
+                    new KeyValuePair<string, string>("password", password),
+                    new KeyValuePair<string, string>("scope", "openid"),
+                    new KeyValuePair<string, string>("client_secret", clientSecret),
+                });
+                var authResult = authClient.PostAsync("https://login.microsoftonline.com/common/oauth2/token", authContent).Result;
+                var success = authResult.StatusCode == HttpStatusCode.OK;
+                if (success)
+                {
+                    var responseContent = authResult.Content.ReadAsStringAsync().Result;
+                    dynamic responseObject = JsonConvert.DeserializeObject(responseContent);
+                    return (string)responseObject.access_token;
+                }
+                else
+                {
+                    return "";
+                }
+            }
+        }
+    }
+}
---
+++ pbi-pub/Program.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.PowerBI.Api.V2;
+using Microsoft.PowerBI.Api.V2.Models;
+using Microsoft.Rest;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pbi_pub
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var command = args[0];
+            var path = args[1];
+            var groupName = args[2];
+            var clientId = args[3];
+            var clientSecret = args[4];
+            var username = args[5];
+            var password = args[6];
+
+            var token = AuthenticationHelper.GetAccessToken(clientId, clientSecret, username, password);
+
+            var tokenCredentials = new TokenCredentials(token, "Bearer");
+            Group group;
+            using (var client = new PowerBIClient(tokenCredentials))
+            {
+                var groups = client.Groups.GetGroups().Value;
+                var query = from g in groups
+                            where g.Name == groupName
+                            select g;
+                group = query.FirstOrDefault();
+                if (group == null)
+                {
+                    group = client.Groups.CreateGroup(new GroupCreationRequest { Name = groupName });
+                }
+
+                var fileName = Path.GetFileName(path);
+                var fileStream = File.OpenRead(path);
+                var result = client.Imports.PostImportFileWithHttpMessage(group.Id, fileStream, fileName, "Overwrite").Result;
+            }
+        }
+    }
+}
