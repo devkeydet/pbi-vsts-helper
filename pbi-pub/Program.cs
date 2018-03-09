c860ca01b334f21c8f9ca9cd9fdf34864baaade3
devkeydet <devkeydet@gmail.com>
2018-03-09 12:00:52 -0500

    support for updateing datasource

--- pbi-pub/Program.cs
+++ pbi-pub/Program.cs
@@ -1,8 +1,12 @@
 ﻿using Microsoft.PowerBI.Api.V2;
 using Microsoft.PowerBI.Api.V2.Models;
 using Microsoft.Rest;
+using System;
 using System.IO;
 using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
 
 namespace pbi_pub
 {
@@ -17,35 +21,82 @@ namespace pbi_pub
             var clientSecret = args[4];
             var username = args[5];
             var password = args[6];
+            var dataSourceUpdates = args[7]; // Example JSON below
+                                             //var dataSourceUpdates =
+                                             //    @"{
+                                             //      ""updateDetails"":[
+                                             //        {
+                                             //          ""connectionDetails"":
+                                             //          {
+                                             //            ""url"": ""https://marcsc-corrmgmt.api.crm.dynamics.com/api/data/v8.2""
+                                             //          },
+                                             //          ""datasourceSelector"":
+                                             //          {
+                                             //            ""datasourceType"": ""OData"",
+                                             //                ""connectionDetails"": {
+                                             //                    ""url"": ""https://cmv9.api.crm.dynamics.com/api/data/v8.2""
+                                             //                }
+                                             //          }
+                                             //        }
+                                             //      ]
+                                             //    }";
 
             var token = AuthenticationHelper.GetAccessToken(clientId, clientSecret, username, password);
 
             var tokenCredentials = new TokenCredentials(token, "Bearer");
             Group group;
-            using (var client = new PowerBIClient(tokenCredentials))
+            using (var pbiClient = new PowerBIClient(tokenCredentials))
             {
-                var groups = client.Groups.GetGroups().Value;
-                var query = from g in groups
-                            where g.Name == groupName
-                            select g;
-                group = query.FirstOrDefault();
+                // Get groups/workspaces
+                var groups = pbiClient.Groups.GetGroups().Value;
+                var groupsQuery = from g in groups
+                                  where g.Name == groupName
+                                  select g;
+                group = groupsQuery.FirstOrDefault();
+                // Create group if it doesn't exist
                 if (group == null)
                 {
-                    group = client.Groups.CreateGroupAsync(new GroupCreationRequest { Name = groupName }).Result;
+                    group = pbiClient.Groups.CreateGroupAsync(new GroupCreationRequest { Name = groupName }).Result;
                 }
 
+                // Import .pbix into group
                 var fileName = Path.GetFileName(path);
-                FileStream fileStream = File.OpenRead(path);
+                var fileStream = File.OpenRead(path);
                 HttpOperationResponse<Import> response;
                 try
                 {
-                    response = client.Imports.PostImportFileWithHttpMessage(group.Id, fileStream, fileName, "Overwrite").Result;
+                    response = pbiClient.Imports.PostImportFileWithHttpMessage(group.Id, fileStream, fileName, "Overwrite").Result;
                 }
-                catch (System.Exception)
+                catch (Exception)
                 {
                     fileStream = File.OpenRead(path);
-                    response = client.Imports.PostImportFileWithHttpMessage(group.Id, fileStream, fileName).Result;
+                    response = pbiClient.Imports.PostImportFileWithHttpMessage(group.Id, fileStream, fileName).Result;
                 }
+
+                // Get the datasource of the updated .pbix
+                var dataSets = pbiClient.Datasets.GetDatasetsInGroupAsync(group.Id).Result;
+                var dataSetsQuery = from d in dataSets.Value
+                                    where d.Name == fileName.Replace(".pbix", "")
+                                    select d;
+                var dataSetId = dataSetsQuery.First().Id;
+
+                // Update the data sources so that they point to the right Dynamics instance.
+                var content = new StringContent(dataSourceUpdates, Encoding.UTF8, "application/json");
+                using (var httpClient = new HttpClient())
+                {
+                    httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+                    //httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                    var result = httpClient.PostAsync(
+                        $"{pbiClient.BaseUri}/v1.0/myorg/groups/{group.Id}/datasets/{dataSetId}/updatedatasources",
+                        content).Result;
+                    if (!result.IsSuccessStatusCode)
+                    {
+                        throw new Exception("failed to update the datasource");
+                    }
+                }
+
+                // TODO: would be great if we could update the data source credentials and then refresh it.
+                // Don't see how to do that with the current API:https://msdn.microsoft.com/en-us/library/mt784652.aspx
             }
         }
     }
